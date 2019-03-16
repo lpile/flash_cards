289cc5526c998b38be9b305980d63f6cb53b3f09
lpile <logan.pile@gmail.com>
2019-03-16 15:20:36 -0600

    Update flashcard_runner file

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -7,7 +7,7 @@ require 'pry'
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-card_4 = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+card_4 = Card.new("How many points is a touchdown in American football", "6", :Sports)
 deck = Deck.new([card_1, card_2, card_3, card_4])
 round = Round.new(deck)
 round.start
