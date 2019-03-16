b5f252a84b7e1eef2091a4c4e4aa9b816462bf0d
lpile <logan.pile@gmail.com>
2019-03-16 12:37:01 -0600

    Update Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,10 +6,12 @@ class Deck
     @cards = cards
   end
 
+  # number of cards in deck
   def count
     @cards.count
   end
 
+  # select cards by category
   def cards_in_category(type)
     @cards.select do |card|
       card.category == type
