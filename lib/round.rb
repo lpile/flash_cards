f1e6fea4e38759e88902293bdfa0ef50ca5a3ae6
lpile <logan.pile@gmail.com>
2019-03-16 15:32:44 -0600

    Update Round class with user interaction

--- lib/round.rb
+++ lib/round.rb
@@ -57,4 +57,34 @@ class Round
     end
     ((number_correct_by_category(category).to_f/@number_in_category.count) * 100.0).round(1)
   end
+
+  def start
+    p "Welcome! You're playing with #{@deck.count} cards"
+    p "-------------------------------------------"
+    p "This is card number #{@current_card + 1} out of #{@deck.count}"
+    p "Question: #{current_card.question}"
+    self.middle
+  end
+
+  def middle
+    guess = gets.chomp.to_s
+    take_turn(guess)
+    p @turns.last.feedback
+    if self.current_card == nil
+      self.end
+    else
+      p "This is card number #{@current_card + 1} out of #{deck.count}"
+      p "Question: #{current_card.question}"
+      self.middle
+    end
+  end
+
+  def end
+    p "************************ GAME OVER! ************************"
+    p "You had #{self.number_correct} correct guess out of #{@turns.count} for a total score of #{self.percent_correct}%"
+    p "Geography - #{percent_correct_by_category(:Geography)}% correct"
+    p "STEM - #{percent_correct_by_category(:STEM)}% correct"
+    p "Sports - #{percent_correct_by_category(:Sports)}% correct"
+    p "************************************************************"
+  end
 end
