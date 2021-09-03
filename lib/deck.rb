914b931cce94670eb5f32f9acdf3842c026f90e3
Logan Pile <loganpile@Logans-MacBook-Pro.local>
2021-09-02 19:46:33 -0600

    Refactor deck_test to deck_spec for change to rspec

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,21 +1,17 @@
 class Deck
-
   attr_reader :cards
 
   def initialize(cards)
     @cards = cards
   end
 
-  # number of cards in deck
   def count
     @cards.count
   end
 
-  # select cards by category
   def cards_in_category(type)
     @cards.select do |card|
       card.category == type
     end
   end
-
 end
